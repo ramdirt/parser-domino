1e77ba6ebe5632ff22eeb2f4c181ad82b148c536
Алексей <ramdirt@mail.ru>
2021-06-03 11:50:11 +0300

    Добавил проверку на копию и скачивание всего раздела

--- domino/download_image.py
+++ domino/download_image.py
@@ -1,28 +1,38 @@
 # -*- coding: utf8 -*-
 from functions import *
 from urllib.request import *
+import os
+import time
 
 setting = Settings()
 
 def main():
-    get_catalog_data(get_html(setting.url_catalog))
+    urls = setting.urls_lever_assembly
+    for url in urls:
+        get_catalog_data(get_html(url))
 
 def get_catalog_data(html):
     soup = BeautifulSoup(html, 'lxml')
     blocks = soup.find_all('div', {'class':'col col-sm-6 col-md-4 col-lg-3'})
 
     for block in blocks[setting.range_min:setting.range_max]:
-        code = block.find('div', {'class': 'code'}).text
+        code = block.find('div', {'class': 'code'}).text.replace(" ", "")
         parts_info = get_page_data(get_html(setting.url_domen + block.find('a').get('href')))
         title = title_catalog_refine(get_html(setting.url_domen + block.find('a').get('href')))
 
         image_url = parts_info['image_url']
-        area_of_use = parts_info['area_of_use']
-        type = parts_info['type_parts']
+        area_of_use = parts_info['area_of_use'][0]
+        type = parts_info['type_parts'].lstrip().rstrip()
 
-        for i in area_of_use:
-            urlretrieve(image_url, f'img/{title}/{i}/{type}/{code}.jpg')
-            print(f'Скачано изображение: {code} в раздел img/{title}/{i}/{type}')
+        if not os.path.isdir(f'img/{title}/{area_of_use}/{type}'):
+            os.makedirs(f'img/{title}/{area_of_use}/{type}')
+
+        if not os.path.isfile(f'img/{title}/{area_of_use}/{type}/{code}.jpg'):
+            time.sleep(0)
+            urlretrieve(image_url, f'img/{title}/{area_of_use}/{type}/{code}.jpg')
+            print(f'Изображение скачано в img/{title}/{area_of_use}/{type} с названием {code}')
+        else:
+            print('Такой файл есть')
 
 def get_page_data(html):
     type_parts = title_parts_refine('title', html)
--- domino/settings.py
+++ domino/settings.py
@@ -1,6 +1,12 @@
 class Settings():
     def __init__(self):
-        self.url_catalog = 'https://www.domino-group.com/tommaselli/en/grips-atv.html'
+        self.urls_lever_assembly = ['https://www.domino-group.com/tommaselli/en/lever-assembly-on-road.html',
+                             'https://www.domino-group.com/tommaselli/en/lever-assembly-off-road.html',
+                             'https://www.domino-group.com/tommaselli/en/lever-assembly-trial.html',
+                             'https://www.domino-group.com/tommaselli/en/lever-assembly-scooter.html',
+                             'https://www.domino-group.com/tommaselli/en/lever-assembly-vintage.html',
+                             'https://www.domino-group.com/tommaselli/en/lever-assembly-speedway.html']
+        self.url_catalog = 'https://www.domino-group.com/tommaselli/en/lever-assembly-on-road.html'
         self.url_domen = 'https://www.domino-group.com'
         self.money = 89
         self.file_save = 'csv/' + self.url_catalog.split('/')[-1][0:-5] + '.csv'
